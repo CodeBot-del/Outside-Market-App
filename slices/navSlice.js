4c98a27312545779079a3e1c02eb0ac802ddcb62
steve Manangu <stevenmanangu360@gmail.com>
2022-10-10 14:46:13 +0300

    keeping up with the redux slices

--- slices/navSlice.js
+++ slices/navSlice.js
@@ -7,5 +7,17 @@ const initialState = {
 }
 
 export const navSlice = createSlice({
-    
+    name: 'nav',
+    initialState,
+    reducer: {
+        setOrigin: (state, action) => {
+            state.origin = action.payload;
+        },
+        setDestination: (state, action) => {
+            state.destination = action.payload;
+        },
+        setTravelTimeInformation: (state, action) => {
+            state.travelTimeInformation = action.payload;
+        }
+    }
 })
