edab7248ef8e05602ad8882d337d8b604ba93be6
steve Manangu <stevenmanangu360@gmail.com>
2022-10-10 17:55:29 +0300

    implementing react navigation

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import {Provider} from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
 import {SafeAreaProvider} from "react-native-safe-area-context";
+import 'react-native-gesture-handler';
+import {NavigationContainer} from "@react-navigation/native";
 
 // 1) Set up redux
 
