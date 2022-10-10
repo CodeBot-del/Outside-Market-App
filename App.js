f02ce23765be5c109f0456123e42835d54e7b797
steve Manangu <stevenmanangu360@gmail.com>
2022-10-10 15:04:10 +0300

    creating the screens

--- App.js
+++ App.js
@@ -2,6 +2,7 @@
 import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
 import {Provider} from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
 
 
@@ -12,10 +13,7 @@ import { store } from "./store";
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-      <Text>Let's build Outside MArket</Text>
-      {/* <StatusBar style="auto" /> */}
-    </View>
+      <HomeScreen />
     </Provider>
     
   );
