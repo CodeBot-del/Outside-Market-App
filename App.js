533fab1d3a8405b31e90bdfd7849a9935549f3d4
steve Manangu <stevenmanangu360@gmail.com>
2022-10-10 14:36:51 +0300

    creating a nav slice

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 // import { StatusBar } from 'expo-status-bar';
+import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
+import {Provider} from "react-redux";
+
+
+// 1) Set up redux
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <Provider store={}>
+      <View style={styles.container}>
       <Text>Let's build Outside MArket</Text>
       {/* <StatusBar style="auto" /> */}
     </View>
+    </Provider>
+    
   );
 }
 
